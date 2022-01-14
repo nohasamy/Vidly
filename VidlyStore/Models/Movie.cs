eb8cfa47fd102bd2a8a3a66895960970417c1da7
noha sami <nohasamy13@gmail.com>
2022-01-14 23:03:18 +0200

    Add Props To Movie And Add Genre And Movie To DB

--- VidlyStore/Models/Movie.cs
+++ VidlyStore/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,20 @@ namespace VidlyStore.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(225)]
         public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        public DateTime AddedDate { get; set; }
+
+        public int NumberInStock { get; set; }
+
+
     }
 }
