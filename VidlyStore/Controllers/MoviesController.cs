36de2193374339ef14df5a1ab5ee772e85995000
noha sami <nohasamy13@gmail.com>
2022-01-15 00:12:43 +0200

    Update DisplayView With Genre And Create DetailsView To Show Movies Details

--- VidlyStore/Controllers/MoviesController.cs
+++ VidlyStore/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,21 +11,34 @@ namespace VidlyStore.Controllers
 {
     public class MoviesController : Controller
     {
-        public ActionResult Display()
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            var movies = GetMovies();
-            return View((object)movies);
+            _context = new ApplicationDbContext();
+        } 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
 
-        public static List<Movie> GetMovies()
+        public ActionResult Display()
         {
-            var movies = new List<Movie>()
-            {
-                new Movie {Id = 1, Name = "Coco"},
-                new Movie {Id = 2, Name = "Soul!"}
-            };
-            return movies;
+            var movies = _context.Movies.Include(m => m.Genre);
+            return View(movies);
         }
+        public ActionResult Details(int? id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+        
+
+
+
 
         // GET: Movies/Random
         public ActionResult Random()
