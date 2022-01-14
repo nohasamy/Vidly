27301e163f2330a592bd4deff627006d001fad4f
noha sami <nohasamy13@gmail.com>
2022-01-14 02:15:35 +0200

    MadeInitialMigration

--- VidlyStore/Models/IdentityModels.cs
+++ VidlyStore/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace VidlyStore.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
