cb9745c686639efb5a53544313f25390e09b3d92
noha sami <nohasamy13@gmail.com>
2022-01-14 22:42:09 +0200

    Populate Customers Birthdate And Update Details View To Display Birthdate And MembershipType ForEach Customer

--- VidlyStore/Models/Customer.cs
+++ VidlyStore/Models/Customer.cs
@@ -13,7 +13,6 @@ namespace VidlyStore.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
