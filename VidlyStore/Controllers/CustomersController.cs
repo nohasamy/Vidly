43083d452661c7f824fdf6b1367c2e339f5b9fd7
noha sami <nohasamy13@gmail.com>
2022-01-14 03:34:24 +0200

    LoadCustomersFromDatabaseAndDisplayMembershipDiscountForThem

--- VidlyStore/Controllers/CustomersController.cs
+++ VidlyStore/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,29 +10,34 @@ namespace VidlyStore.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Display()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType);
+
             return View(customers);
         }
 
         public ActionResult Details(int? id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
 
         }
-        public List<Customer> GetCustomers()
-        {
-            var customers = new List<Customer>()
-            {
-                new Customer { Id = 1, Name = "Noha" },
-                new Customer { Id = 2, Name = "Momen" }
-            };
-            return customers;
-        }
+       
     }
 }
---
+++ VidlyStore/Migrations/202201140132395_LoadCustomersFromDatabaseAndDisplayMembershipDiscountForThem.cs
@@ -0,0 +1,16 @@
+namespace VidlyStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LoadCustomersFromDatabaseAndDisplayMembershipDiscountForThem : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
